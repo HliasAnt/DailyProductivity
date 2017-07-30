cc981b6aea8c8af32ab93c65da3d603f60956ae6
hlias antonopoulos <hlias_antonopoulos@hotmail.com>
2017-07-30 15:32:53 +0300

    Add project files.

---
+++ DailyProductivity/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace DailyProductivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.rbHeaderYes = new System.Windows.Forms.RadioButton();
+            this.rbHeaderNo = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Account";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(208, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(60, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(287, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(41, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ΚΛΑΣΕΡ",
+            "ΦΑΚΕΛΟΣ",
+            "BOX FILE",
+            "ΚΟΥΤΙ MADDOG",
+            "ΧΥΜΑ ΥΛΙΚΟ"});
+            this.comboBox1.Location = new System.Drawing.Point(352, 83);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 189);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(777, 191);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Εγγραφες";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "κιβωτια";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(379, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ειδος αρχειου";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(66, 130);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(407, 36);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "πληροφοριες";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "SET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(503, 84);
+            this.dateTimePicker1.MinDate = new System.DateTime(1990, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(98, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            this.dateTimePicker1.Value = new System.DateTime(2017, 7, 14, 0, 0, 0, 0);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(614, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(641, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // rbHeaderYes
+            // 
+            this.rbHeaderYes.AutoSize = true;
+            this.rbHeaderYes.Location = new System.Drawing.Point(135, 26);
+            this.rbHeaderYes.Name = "rbHeaderYes";
+            this.rbHeaderYes.Size = new System.Drawing.Size(85, 17);
+            this.rbHeaderYes.TabIndex = 15;
+            this.rbHeaderYes.TabStop = true;
+            this.rbHeaderYes.Text = "radioButton1";
+            this.rbHeaderYes.UseVisualStyleBackColor = true;
+            // 
+            // rbHeaderNo
+            // 
+            this.rbHeaderNo.AutoSize = true;
+            this.rbHeaderNo.Location = new System.Drawing.Point(226, 26);
+            this.rbHeaderNo.Name = "rbHeaderNo";
+            this.rbHeaderNo.Size = new System.Drawing.Size(85, 17);
+            this.rbHeaderNo.TabIndex = 16;
+            this.rbHeaderNo.TabStop = true;
+            this.rbHeaderNo.Text = "radioButton2";
+            this.rbHeaderNo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 417);
+            this.Controls.Add(this.rbHeaderNo);
+            this.Controls.Add(this.rbHeaderYes);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Productivity";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RadioButton rbHeaderYes;
+        private System.Windows.Forms.RadioButton rbHeaderNo;
+    }
+}
+
---
+++ DailyProductivity/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+namespace DailyProductivity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        DataTable table;
+        DataRow dr;
+
+        // create Datatable with 6 columns and add new rows with imported data from textboxes
+        public DataTable GetResultsTable()
+        {
+            if (table == null)
+            {
+                table = new DataTable();
+                table.Columns.Add("DATE".ToString());
+                table.Columns.Add("ACCOUNT".ToString());
+                table.Columns.Add("ΕΓΓΡΑΦΕΣ".ToString());
+                table.Columns.Add("ΕΙΔΟΣ ΑΡΧΕΙΟΥ".ToString());
+                table.Columns.Add("ΚΙΒΩΤΙΑ".ToString());
+                table.Columns.Add("ΠΛΗΡΟΦΟΡΙΕΣ".ToString());
+            }
+            dr = table.NewRow();
+            dr["ACCOUNT"] = textBox1.Text;
+            dr["ΕΓΓΡΑΦΕΣ"] = textBox2.Text;
+            dr["ΚΙΒΩΤΙΑ"] = textBox3.Text;
+            dr["ΕΙΔΟΣ ΑΡΧΕΙΟΥ"] = comboBox1.Text;
+            dr["DATE"] = dateTimePicker1.Text;
+            dr["ΠΛΗΡΟΦΟΡΙΕΣ"] = textBox4.Text;
+            table.Rows.Add(dr);
+            return table;
+        }
+
+        //adds the table data in Gridview
+        public void gridview()
+        {
+            dataGridView1.DataSource = GetResultsTable();
+        }
+
+        // adds a new row with every click with data from textboxes
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gridview();
+
+        }
+
+        //saves the datagridview data in excel file
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Documents (*.xls)|*.xls";
+            sfd.FileName = "export.xls";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                //ToCsV(dataGridView1, @"c:\export.xls");
+                ToCsV(dataGridView1, sfd.FileName); // Here dataGridview1 is your grid view name
+            }
+
+        }
+
+        private void ToCsV(DataGridView dGV, string filename)
+        {
+            string stOutput = "";
+            // Export titles:
+            string sHeaders = "";
+
+            for (int j = 0; j < dGV.Columns.Count; j++)
+                sHeaders = sHeaders.ToString() + Convert.ToString(dGV.Columns[j].HeaderText) + "\t";
+            stOutput += sHeaders + "\r\n";
+            // Export data.
+            for (int i = 0; i < dGV.RowCount - 1; i++)
+            {
+                string stLine = "";
+                for (int j = 0; j < dGV.Rows[i].Cells.Count; j++)
+                    stLine = stLine.ToString() + Convert.ToString(dGV.Rows[i].Cells[j].Value) + "\t";
+                stOutput += stLine + "\r\n";
+            }
+            Encoding utf8 = Encoding.GetEncoding(1253);
+            byte[] output = utf8.GetBytes(stOutput);
+            FileStream fs = new FileStream(filename, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(output, 0, output.Length); //write the encoded file
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+        }
+
+        private string Excel07ConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+        private string Excel03ConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string filePath = openFileDialog1.FileName;
+            string extension = Path.GetExtension(filePath);
+            string header = rbHeaderYes.Checked ? "YES" : "NO";
+            string conStr, sheetName;
+
+            conStr = string.Empty;
+            switch (extension)
+            {
+
+                case ".xls": //Excel 97-03
+                    conStr = string.Format(Excel03ConString, filePath, header);
+                    break;
+
+                case ".xlsx": //Excel 07
+                    conStr = string.Format(Excel07ConString, filePath, header);
+                    break;
+            }
+
+            //Get the name of the First Sheet.
+            using (OleDbConnection con = new OleDbConnection(conStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    DataTable dtExcelSchema = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                    con.Close();
+                }
+            }
+
+            //Read Data from the First Sheet.
+            using (OleDbConnection con = new OleDbConnection(conStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    using (OleDbDataAdapter oda = new OleDbDataAdapter())
+                    {
+                        DataTable dt = new DataTable();
+                        cmd.CommandText = "SELECT * From [" + sheetName + "]";
+                        cmd.Connection = con;
+                        con.Open();
+                        oda.SelectCommand = cmd;
+                        oda.Fill(dt);
+                        con.Close();
+
+                        //Populate DataGridView.
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
+        }
+    }
+}
